e18081b8911c0d5baa16206489efbd5fdf43af47
asemic horizon <42865953+asemic-horizon@users.noreply.github.com>
2019-09-11 12:25:15 -0300

    Minimal setup.py for pip installation
    (fixed some commas)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='Kodhy',
       version='0.01',
-      description="Kodi's Hy utility functions and macros"
+      description="Kodi's Hy utility functions and macros",
       author='Kodi Arfer',
       author_email='',
       license='GPL 3',
