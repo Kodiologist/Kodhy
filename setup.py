7a9cf16b6cc75aaa86d1cde3253ce1c494937b00
asemic horizon <42865953+asemic-horizon@users.noreply.github.com>
2019-09-11 12:19:51 -0300

    Minimal setup.py to enable pip install git+

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Kodhy',
+      version='0.01',
+      description="Kodi's Hy utility functions and macros"
+      author='Kodi Arfer',
+      author_email='',
+      license='GPL 3',
+      zip_safe=False)
+
