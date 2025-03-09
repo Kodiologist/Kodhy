52bc7dc800e4695a5cb424fc21739de6418a12b2
Kodi Arfer <git@arfer.net>
2025-03-09 13:54:25 -0400

    Updated the test configuration.

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
-import hy
-import pytest
+import hy, pytest
 
-def pytest_collect_file(parent, path):
-    if (path.ext == ".hy" and path.basename != "__init__.hy"):
-        return pytest.Module.from_parent(parent, fspath=path)
+def pytest_collect_file(file_path, parent):
+    if file_path.name.startswith('test_') and file_path.suffix == '.hy':
+        return pytest.Module.from_parent(parent, path = file_path)
